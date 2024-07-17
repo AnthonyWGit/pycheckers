68c4ffd075ed003d9dd313e424fb7b1a13979903
AnthonyWGit <anthony.wetzstein@gmail.com>
2024-07-17 14:29:48 +0200

    the board is drawn

--- main.py
+++ main.py
@@ -25,17 +25,29 @@ class MainWindow:
         board_borders = tk.Canvas(self.window, borderwidth=1)   
         board_borders.grid(row = 1, column = 0, sticky = "ew", columnspan= 8, rowspan= 8)
         row = 8
+        col = 8
         i = 0
+        j = 0
         x = 30
         y = 30
-        for i in range(row):
-            #pair number
-            if(i % 2 == 0):
-                #first two numbers for argument are coordinates at top left and rest on px after bottom right 
-                # https://web.archive.org/web/20181223164027/http://infohost.nmt.edu/tcc/help/pubs/tkinter/web/create_rectangle.html
-                board_borders.create_rectangle(i * x,0,(i + 1) * x,y,fill='black')
+        for j in range(col):
+            if (j % 2 == 0):
+                for i in range(row):
+                    #pair number
+                    if(i % 2 == 0):
+                        #first two numbers for argument are coordinates at top left and rest on px after bottom right 
+                        # https://web.archive.org/web/20181223164027/http://infohost.nmt.edu/tcc/help/pubs/tkinter/web/create_rectangle.html
+                        board_borders.create_rectangle(i * x,j * y,(i + 1) * x,(j + 1) *y,fill='black')
+                    else:
+                        board_borders.create_rectangle(i * x,j * y,(i + 1) * x,(j + 1) *y,fill='white')
             else:
-                board_borders.create_rectangle(i * x,0,(i + 1) * x,y,fill='white')
+                for i in range(row):
+                    #pair number
+                    if(i % 2 == 0):
+                        board_borders.create_rectangle(i * x,j * y,(i + 1) * x,(j + 1) *y,fill='white')
+                    else:
+                        board_borders.create_rectangle(i * x,j * y,(i + 1) * x,(j + 1) *y,fill='black')
+
 
 
 #Create the window and let it run until user quits 
