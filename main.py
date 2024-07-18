1fafe5245c4465b0ce3714ab4afb4e4ac1d6d9d4
AnthonyWGit <anthony.wetzstein@gmail.com>
2024-07-18 11:34:43 +0200

    moving x y of drawboard in construct so it can be used in place_pawns

--- main.py
+++ main.py
@@ -25,38 +25,43 @@ class Board:
         self.board = tk.Canvas(window, borderwidth=1)
         #Drawing the grid
         self.board.grid(row = 1, column = 0, sticky = "ew", columnspan= 8, rowspan= 8)
+        self.row = 8
+        self.col = 8
+        self.x = 30
+        self.y = 30
         self.draw_board()
 
+    def start_place_pawns(self):
+        print('Hi')
+
     def draw_board(self):
-        row = 8
-        col = 8
-        x = 30
-        y = 30
-        for j in range(col):
+        for j in range(self.col):
             if (j % 2 == 0):
-                for i in range(row):
+                for i in range(self.row):
                     #pair number
                     if(i % 2 == 0):
                         #first two numbers for argument are coordinates at top left and rest on px after bottom right 
                         # https://web.archive.org/web/20181223164027/http://infohost.nmt.edu/tcc/help/pubs/tkinter/web/create_rectangle.html
-                        self.board.create_rectangle(i * x,j * y,(i + 1) * x,(j + 1) * y,fill='black')
+                        self.board.create_rectangle(i * self.x,j * self.y,(i + 1) * self.x,(j + 1) * self.y,fill='black')
                     else:   
-                        self.board.create_rectangle(i * x,j * y,(i + 1) * x,(j + 1) *y,fill='white')
+                        self.board.create_rectangle(i * self.x,j * self.y,(i + 1) * self.x,(j + 1) *self.y,fill='white')
             else:
-                for i in range(row):
+                for i in range(self.row):
                     #pair number
                     if(i % 2 == 0):
-                        self.board.create_rectangle(i * x,j * y,(i + 1) * x,(j + 1) *y,fill='white')
+                        self.board.create_rectangle(i * self.x,j * self.y,(i + 1) * self.x,(j + 1) *self.y,fill='white')
                     else:
-                        self.board.create_rectangle(i * x,j * y,(i + 1) * x,(j + 1) *y,fill='black')
+                        self.board.create_rectangle(i * self.x,j * self.y,(i + 1) * self.x,(j + 1) *self.y,fill='black')
 
 class Cell:
     def __init__(self):
-        
-        pass
+        #coordinates
+        self.x = x
+        self.y = y
+        self.free = True
 class Pawn:
     def __init__(self):
-
+        self.pox = pox
 
 #Create the window and let it run until user quits 
 root = tk.Tk()
