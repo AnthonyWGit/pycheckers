d8104eb2c3146e285420769be0912b4cf65832ac
AnthonyWGit <anthony.wetzstein@gmail.com>
2024-09-11 14:48:09 +0200

    changing geometry and adding a menu

--- MainWindow.py
+++ MainWindow.py
@@ -1,20 +1,13 @@
 import tkinter as tk
+from RulesWindow import RulesWindow
+from AproposWindow import AproposWindow
 from Board import Board
 
 
 class MainWindow:
     def __init__(self, window):
         self.window = window
-        #Create window at the center of screen
-        window_height = 600
-        window_width = 600
-        screen_height = window.winfo_screenheight()
-        screen_width = window.winfo_screenwidth()
-        #center of screen
-        center_x = int(screen_width/2 - window_width / 2)
-        center_y = int(screen_height/2 - window_height / 2)
         window.title("pyCheckers")
-        window.geometry(f'{window_height}x{window_width}+{center_x}+{center_y}')
         self.board = Board(window)
         self.turn_label = tk.Label(window, text=f"")
         self.turn_label.grid(row=0, column=0)
@@ -28,4 +21,33 @@ class MainWindow:
         self.button.grid(row=12, column=0)
         self.button = tk.Button(window, text="Debug Pawns", command=self.board.log_debug_pawns)
         self.button.grid(row=12, column=1)
+        # Create menu
+        self.menu = tk.Menu(window)
+        window.config(menu=self.menu)
+        self.rules_menu = tk.Menu(self.menu, tearoff=0)
+        self.menu.add_cascade(label="Menu", menu=self.rules_menu)
+        self.rules_menu.add_command(label="Show Rules", command=self.show_rules)
+        self.rules_menu.add_command(label="About", command=self.show_apropos)
+        # Center the window : separate function to pack calculations
+        self.center_window()
+
+    def center_window(self):
+        #Get the window size when everything is packed
+        self.window.update_idletasks()
+        #Get H/W of tkinterwindow
+        window_width = self.window.winfo_width()
+        window_height = self.window.winfo_height()
+        #Get screen size
+        screen_width = self.window.winfo_screenwidth()
+        screen_height = self.window.winfo_screenheight()
+        center_x = int(screen_width / 2 - window_width / 2)
+        center_y = int(screen_height / 2 - window_height / 2)
+        #Using calc values for proper geometry
+        self.window.geometry(f'{window_width}x{window_height}+{center_x}+{center_y}')
+
+    def show_rules(self):
+        rules_window = RulesWindow(self.window)
+
+    def show_apropos(self):
+        apropos_window = AproposWindow(self.window)
 
