f20b7b652e558635dad66ee69c5ebd80c1c2cf00
AnthonyWGit <anthony.wetzstein@gmail.com>
2024-09-23 14:42:24 +0200

    moved the canvas config and griding to mainwindow

--- Board.py
+++ Board.py
@@ -7,10 +7,8 @@ class Board:
     def __init__(self, window):
         #bord is a Canvas widget from Tkinter class
         self.canvas = tk.Canvas(window, borderwidth=1)
-        self.window = window
         self.last_clicked_cell = None
         #Drawing the grid
-        self.canvas.grid(row = 4, column = 0, sticky = "ew", columnspan= 8, rowspan= 8)
         self.row = 8
         self.col = 8
         #x and y size of square
